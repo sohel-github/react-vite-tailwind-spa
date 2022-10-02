d908b8fc95752983b5aacb5b8f01f319a215c240
Sohel <tosohel22@gmail.com>
2022-10-02 17:04:31 +0600

    Vite react tailwinds spa

---
+++ src/components/CTA.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from '../style'
+import Button from './Button'
+
+const CTA = () => (
+  <section className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}>
+    <div className='flex-1 flex flex-col'>
+      <h2 className={styles.heading2}>Lets try our service now</h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore laboriosam deleniti pariatur quia dignissimos illo.</p>
+    </div>
+    <Button className={`${styles.flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-10`} />
+  </section>
+);
+
+export default CTA
